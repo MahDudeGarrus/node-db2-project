4fbaa045984c3514c29810859dfcc2e9eeb9330f
Your Name <you@example.com>
2021-09-25 17:20:46 -0600

    seeds for cars created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
-// STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'Toyota',
+        model: 'Prius',
+        milage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'Toyota',
+        model: 'Corolla',
+        milage: 115000,
+        title: 'salvage'
+    },
+    {
+        vin: '1111111111111',
+        make: 'Ford',
+        model: 'Focus',
+        milage: 215000
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
